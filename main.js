b83950b1c7e22216dbbd3410528a4dc3b73eb825
Daewik <75206835+Daewik@users.noreply.github.com>
2022-04-03 15:50:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function preload()
+{}
+
+function setup()
+{
+    canvas = createCanvas(1000,400);
+    canvas.center();
+}
+
+function draw()
+{
+    background('white');
+}
+
+function clear()
+{
+    background("white");
+}
